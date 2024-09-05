9f3b7fac94e7d52436689b0fc9cd8806876e0bde
Ernestoc14 <descreed34@hotmail.com>
2024-09-04 23:36:16 -0500

    Feat: Improve API structure and add movie endpoints GET

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Interestellar",
+        "overview": "Space",
+        "year": "2014",
+        "rating": 9.3,
+        "category": "Ficcion"
+    },{
+        "id": 3,
+        "title": "Garfield",
+        "overview": "Cat movie",
+        "year": "2023",
+        "rating": 8.2,
+        "category": "Comic"
+    }
+]
 
 # fastapi dev main.py
 app = FastAPI()
+app.title = "Mi primera API con FastAPI"
 
-@app.get("/")
+@app.get("/", tags= ["home"])
+
 def read_root():
-    return {"Fast Api is Running"}
+    return HTMLResponse("<h2>HOLA</h2>")
 
+# Get all movies
+@app.get("/movies", tags=["movies"])
+def getMovies():
+    return movies
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, quote: Union[str, None] = None):
-    return {"item_id": item_id, "quote": quote}
+# Get one movie by id DYNAMIC ROUTING
+@app.get("/movies/{id}", tags=["movies"])
+def getMovieById(id: int):
+    return [movie for movie in movies if movie["id"] == id]
+
