bfeca13647ca29da4f1e1417dfaab2962fb5f965
Ernestoc14 <descreed34@hotmail.com>
2024-09-08 23:48:50 -0500

    Feat: Filter using Query Params

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ movies = [
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
         "year": "2009",
         "rating": 7.8,
-        "category": "Acción"
+        "category": "Action",
     },
     {
         "id": 2,
@@ -17,33 +17,48 @@ movies = [
         "overview": "Space",
         "year": "2014",
         "rating": 9.3,
-        "category": "Ficcion"
-    },{
+        "category": "Action",
+    },
+    {
         "id": 3,
         "title": "Garfield",
         "overview": "Cat movie",
         "year": "2023",
         "rating": 8.2,
-        "category": "Comic"
-    }
+        "category": "Comic",
+    },
 ]
 
 # fastapi dev main.py
 app = FastAPI()
 app.title = "Mi primera API con FastAPI"
 
-@app.get("/", tags= ["home"])
 
+@app.get("/", tags=["home"])
 def read_root():
     return HTMLResponse("<h2>HOLA</h2>")
+
 
 # Get all movies
 @app.get("/movies", tags=["movies"])
 def getMovies():
     return movies
 
+
 # Get one movie by id DYNAMIC ROUTING
 @app.get("/movies/{id}", tags=["movies"])
 def getMovieById(id: int):
     return [movie for movie in movies if movie["id"] == id]
 
+
+# Query Params
+@app.get("/movies/", tags=["movies"])
+def getMoviesByCategory(category: str, year: int):
+    category = [movie for movie in movies if movie["category"] == category and movie["year"] == year]
+    return category
+
+
+# Delete
+# @app.delete("/movies/{id}", tags=["movies"])
+# def deleteMovieById(id : int):
+#     return
